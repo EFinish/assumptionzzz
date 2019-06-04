cbeb64cbbb202018ae63ac126a576bcf2ffecb6a
evritt <evrittf@gmail.com>
2019-06-04 16:26:29 +0300

    not exactly right, but wip

--- client/components/display/stack.vue
+++ client/components/display/stack.vue
@@ -3,7 +3,8 @@
 		<template v-for="(statement, statementIndex) in stack">
 			{{ statementIndex + 1 }}. <stack-row :key="statementIndex" :statement="statement"></stack-row>
 		</template>
-		<!-- <p>True?: {{ calcTruthValue }}</p> -->
+		<p>Chain Valid?: {{ calcValidValue ? "VALID" : "INVALID" }}</p>
+		<p><strong>Chain True?:</strong> {{ calcTruthValue ? "TRUE" : "FALSE" }}</p>
 	</div>
 </template>
 
@@ -18,6 +19,26 @@ export default {
 		stack: {
 			type: Array,
 			default: () => []
+		}
+	},
+	calculated: {
+		calcValidValue: function () {
+			const invalid = this.stack.find(function(statement) {
+				return statement.valid_value !== false;
+			});
+
+			console.log(!invalid)
+
+			return !invalid;
+		},
+		calcTruthValue: function () {
+			// invalid statement = statement false
+			if (this.calcValidValue() === false) {
+				return false;
+			}
+
+			// todo put in truth calc logic
+			return false;
 		}
 	}
 }
