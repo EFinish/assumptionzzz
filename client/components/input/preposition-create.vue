2674267ba156a613c450cbb4d38b1e1b5ae14d95
EFinish <EFinish@gmail.com>
2019-06-18 00:02:28 +0300

    aaahhhhhhhhhhhh

--- client/components/input/preposition-create.vue
+++ client/components/input/preposition-create.vue
@@ -3,21 +3,28 @@
 		<p>New Proposition</p>
 		<div>
 			<label for="proposition-type">type</label>
-			<select id="proposition-type" v-model="form.selected_type_id" @change="setSelectedPrepositionType($event)">
+			<select id="proposition-type" v-model="form.selected_type_id" @change="setSelectedPrepositionType()">
 				<option v-for="(preposition, index) in preposition_types" :key="index" :value="preposition.id">
 					{{ preposition.label }}
 				</option>
 			</select>
 		</div>
-		<div v-for="(statement, index) in selected_statements" :key="index">
-			<label for="preposition-statement">statement part {{ index }}</label>
-			<!-- <select id="preposition-statement" @change="">
-				<option v-for="(preposition, index) in preposition_types" :key="index" :value="preposition.id">
-					{{ preposition.label }}
-				</option>
-			</select> -->
+ 
+ 		<div v-if="selected_type !== null">
+			<div v-for="(index) in selected_type.amount_statements" :key="index">
+				<label for="set-statement">set statement {{ index }}</label>
+				<select id="set-statement" v-model="form.selected_statement_id" @change="setSelectedStatement(index)">
+					<option v-for="(statement, index) in statement_stack" :key="index" :value="statement.id">
+						{{ statement.description }}
+					</option>
+				</select>
+			</div>
 		</div>
-		<button type="button" @click="submit">Add to Proposition Stack</button>
+
+		<!-- <div v-for="(statement, index) in selected_statements" :key="index">
+			<label for="preposition-statement">statement part {{ index }}</label>
+		</div>
+		<button type="button" @click="submit">Add to Proposition Stack</button> -->
 	</div>
 </template>
 
@@ -43,24 +50,57 @@
 			return {
 				form: {
 					selected_type_id: null,
-					selected_statements: []
-				}
+					selected_statement_id: null,
+				},
+				selected_type: null,
+				selected_statements: []
+			}
+		},
+		computed: {
+			tear: () => {
+				return this.selected_type.amount_statement;
 			}
 		},
 		methods: {
 			setSelectedPrepositionType: function() {
-				//TODO make this more efficiently
 				for (let i = 0; i < this.preposition_types.length; i++) {
-					// console.log('check', this.form.selected_type_id, this.preposition_types[i].id);
 					if (this.form.selected_type_id === this.preposition_types[i].id) {
-						let blankSelectedStatements = [];
-						for (let k = 0; k < this.preposition_types[i].amount_statement; k++) {
-							blankSelectedStatements.push({});
-						}
-						this.selected_statements = blankSelectedStatements;
-						continue;
+						console.log('check', this.form.selected_type_id, this.preposition_types[i].id);
+						this.selected_type = this.preposition_types[i];
+						break;
+				// 		let blankSelectedStatements = [];
+				// 		for (let k = 0; k < this.preposition_types[i].amount_statement; k++) {
+				// 			blankSelectedStatements.push({});
+				// 		}
+				// 		this.selected_statements = blankSelectedStatements;
+				// 		continue;
 					}
 				}
+
+				//TODO make this more efficiently
+				// for (let i = 0; i < this.preposition_types.length; i++) {
+				// 	// console.log('check', this.form.selected_type_id, this.preposition_types[i].id);
+				// 	if (this.form.selected_type_id === this.preposition_types[i].id) {
+				// 		let blankSelectedStatements = [];
+				// 		for (let k = 0; k < this.preposition_types[i].amount_statement; k++) {
+				// 			blankSelectedStatements.push({});
+				// 		}
+				// 		this.selected_statements = blankSelectedStatements;
+				// 		continue;
+				// 	}
+				// }
+			},
+			setSelectedStatement: function (index) {
+				for (let i = 0; i < 20; i++)
+				// for (let i = 0; i < this.statement_stack.length; i++) {
+					// if (this.statement_stack[i].id === this.form.selected_statement_id) {
+						// this.selected_statements[index] = this.statement_stack[i];
+				// 		break;
+					// }
+				}
+			},
+			blog: () => {
+
 			},
 			submit: function () {
 				console.log(this.form);
