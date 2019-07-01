778155e0d3c12b1f1c1a3f04a93ec99e14afb8d9
evritt <evrittf@gmail.com>
2019-07-01 14:36:38 +0300

    introduced backend, better for long term development, dont want to juggle around immutable types when it is just going to be overwritten by api returns anyways, starting the basic api now. Access of project should now happen at http://localhost:54321 as well as having the client changes watched for.

---
+++ server/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	data, err := ioutil.ReadFile("server/index.html")
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Length", fmt.Sprint(len(data)))
+	fmt.Fprint(w, string(data))
+}
+
+func clientBundleHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/javascript")
+	w.WriteHeader(http.StatusOK)
+	data, err := ioutil.ReadFile("build/client.js")
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Length", fmt.Sprint(len(data)))
+	fmt.Fprint(w, string(data))
+}
+
+func main() {
+  http.HandleFunc("/", rootHandler)
+  http.HandleFunc("/client.js", clientBundleHandler)
+	log.Fatal(http.ListenAndServe(":54321", nil))
+}
