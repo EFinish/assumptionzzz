9202d29fcb836fc5dfe953de24219dca74367292
EFinish <EFinish@gmail.com>
2019-11-24 15:18:06 +0200

    server running via docker compose

--- server/ass-server.js
+++ server/ass-server.js
@@ -4,15 +4,17 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-const statementsModel = require('./models/statements');
+// const statementsModel = require('./models/statements');
 
 router.get('/statements', async (ctx, next) => {
-	const allStatments = await statementsModel().getAllStatements();
+	// const allStatments = await statementsModel().getAllStatements();
 
-	ctx.body = allStatments;
-	return next();
+	// ctx.body = allStatments;
+	// return next();
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(425);
+app.listen(425, function() {
+	console.log('ass-server listening on 425! .....')
+});
