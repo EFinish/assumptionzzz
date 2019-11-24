b1ecf1dbb35cc1618dce1185152aa249530c986e
EFinish <EFinish@gmail.com>
2019-11-24 17:04:41 +0200

    it cannot connect to the db for some reason...

--- server/config/knexfile.js
+++ server/config/knexfile.js
@@ -2,12 +2,13 @@ module.exports = {
     client: 'mysql2',
     connection: {
         host : process.env.ASS_DB_URL,
+        port: 3308,
         user : process.env.ASS_DB_USERNAME,
         password : process.env.ASS_DB_PASSWORD,
         database: process.env.ASS_DB_DATABASE
     },
     migrations: {
-        directory: '../migrations',
+        directory: 'migrations',
         tableName: 'migrations'
     }
 };
