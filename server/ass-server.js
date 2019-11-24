deee45e5db5559a093019fa828485779401ff24b
EFinish <EFinish@gmail.com>
2019-11-24 15:25:30 +0200

    hot reloading with nodemon awww yeeee

--- server/ass-server.js
+++ server/ass-server.js
@@ -4,13 +4,16 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-// const statementsModel = require('./models/statements');
+const statementsModel = require('./models/statements');
 
 router.get('/statements', async (ctx, next) => {
 	// const allStatments = await statementsModel().getAllStatements();
 
-	// ctx.body = allStatments;
-	// return next();
+	ctx.body = {
+		success: true,
+		// allStatments
+	};
+	return next();
 });
 
 app.use(router.routes()).use(router.allowedMethods());
